a743552849249629bf7d99c932fae8dd9739e7eb
dkleidonaris <dkleidonaris@gmail.com>
2025-06-08 23:39:53 +0300

    set user fields to not required because they'll be provided after initial registration auto hash password before saving to DB

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,30 @@
 const mongoose = require("mongoose")
+const bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema(
     {
-        firstName: {type: String, required:true},
-        lastName: {type: String, required:true},
-        email: {type: String, required:true, unique:true},
-        hashedPassword: {type: String, required:true},
-        age: {type: Number, required:true},
-        gender: {type: String, required:true},
-        weight: {type: Number, required:true},
-        height: {type: Number, required:true},
+        firstName: { type: String, },
+        lastName: { type: String },
+        email: { type: String, required: true, unique: true },
+        hashedPassword: { type: String, required: true },
+        age: { type: Number },
+        gender: { type: String },
+        weight: { type: Number },
+        height: { type: Number },
     }
 );
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('hashedPassword')) return next(); // skip if not modified
+
+    try {
+        const saltRounds = 10;
+        const hashed = await bcrypt.hash(this.hashedPassword, saltRounds);
+        this.hashedPassword = hashed;
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
 
 module.exports = mongoose.model("user", userSchema);
