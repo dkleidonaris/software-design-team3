0cccb5fde87f785a956bff2b6c84cf9d56d342b9
dkleidonaris <dkleidonaris@gmail.com>
2025-06-08 23:38:08 +0300

    use auth middleware in users test route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const { authMiddleware } = require('../middleware/authMiddleware');
 
 //gia na testaroume mono - dinei olous tous users
-router.get('/', async (req, res) => {
+router.get('/', authMiddleware, async (req, res) => {
   try {
     const users = await User.find();
     res.status(200).json(users);
