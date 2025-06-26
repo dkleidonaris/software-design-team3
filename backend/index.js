d545651662da477b7ffaeee59251648a17ea191c
dkleidonaris <dkleidonaris@gmail.com>
2025-06-26 21:13:29 +0300

    Added meal routes Added auth middleware Added auth routes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './../.env' });
+require('dotenv').config();
 const express = require("express");
 const cors = require('cors');
 const app = express();
@@ -6,15 +6,19 @@ const app = express();
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
 const authRoutes = require("./routes/auth");
+const mealRoutes = require("./routes/meal");
 
 const { apiRateLimiter } = require('./middleware/apiRateLimiter');
+const { authMiddleware } = require('./middleware/authMiddleware');
 
 //ενεργοποίηση του cors για handling διαφορετικών Ports
 app.use(cors());
 app.use(express.json());
 app.use('/api', apiRateLimiter);
-app.use("/api/users", userRoutes);
+
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/meals", authMiddleware, mealRoutes);
 
 mongoose.connect(process.env.MONGO_URI).then(() => console.log("Database connection successfull")).catch((err) => {
     console.log(err);
