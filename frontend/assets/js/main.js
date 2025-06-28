ff7a96635ce1553d3d0dab8f25a8a8282a91aa7c
dkleidonaris <dkleidonaris@gmail.com>
2025-06-28 18:02:55 +0300

    Auto use token for ajax requests Set favicon for all pages

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -2,6 +2,15 @@ import $ from 'jquery';
 window.$ = $;
 import 'bootstrap';
 
+$.ajaxSetup({
+    beforeSend: function (xhr) {
+        const token = localStorage.getItem('token');
+        if (token) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        }
+    }
+});
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../css/main.css';
@@ -12,9 +21,20 @@ const API_URL = import.meta.env.VITE_API_URL;
 import { checkAuthStatus, bindLoginHandler, bindLogoutHandler } from './auth.js';
 
 async function init() {
+    setFavicon('/assets/img/favicon.ico');
     await loadTemplates();
     bindLogoutHandler();
     await checkAuthStatus();
+}
+
+function setFavicon(url) {
+    let link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+    }
+    link.href = url;
 }
 
 $(document).ready(function () {
