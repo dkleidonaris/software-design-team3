7439e29f0dc7d3273bb31b5427b11137c44e99da
dkleidonaris <dkleidonaris@gmail.com>
2025-06-28 16:21:39 +0300

    Added register functionality

--- frontend/assets/js/include.js
+++ frontend/assets/js/include.js
@@ -8,15 +8,13 @@ export function loadTemplates() {
         const component = $(this).data('include');
         const self = $(this);
 
-        // Δημιουργούμε Promise για κάθε AJAX request
         const p = $.get(`/components/${component}.html`).then(html => {
-            // Αντικαθιστούμε το στοιχείο με το HTML του component
+
             self.replaceWith(html);
         });
 
         promises.push(p);
     });
 
-    // Επιστρέφουμε ένα Promise που ολοκληρώνεται όταν ολοκληρωθούν όλα
     return Promise.all(promises);
 }
---
+++ frontend/assets/js/register.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+const API_URL = import.meta.env.VITE_API_URL;
+
+$('form').on('submit', function (event) {
+    event.preventDefault();
+
+    const data = {
+        firstName: $(this).find('#firstName').val(),
+        lastName: $(this).find('#lastName').val(),
+        email: $(this).find('#email').val(),
+        hashedPassword: $(this).find('#password').val(),
+        age: $(this).find('#age').eq(0).val(),
+        gender: $(this).find('#gender').val(),
+        weight: $(this).find('#weight').val(),
+        height: $(this).find('#height').val(),
+    };
+
+    $.ajax({
+        url: API_URL + '/users',
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: function (response) {
+            alert('Account created successfully!');
+            window.location.href = '/login.html';
+        },
+        error: function (xhr, status, error) {
+            const parsed = JSON.parse(xhr.responseText);
+            alert(parsed.error);
+        }
+    });
+});
